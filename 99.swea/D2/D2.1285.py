4881027e0d83555bfbdaf290928446718ebbba32
yunjaeeun <yunjaeeun12@naver.com>
2024-07-23 17:29:33 +0900

    24.07.23 공부내용
    1285. 아름이의 돌던지기(D2)

---
+++ 99.swea/D2/D2.1285.py
@@ -0,0 +1,17 @@
+T = int(input())
+
+for i in range(T):
+    input()
+    arr = list(map(abs, map(int, input().split())))
+
+    min_range = min(arr)
+
+    min_count = 0
+
+    for value in arr:
+        if value == min_range:
+            min_count += 1
+    
+    print(f"#{i + 1} {min_range} {min_count}")
+
+    

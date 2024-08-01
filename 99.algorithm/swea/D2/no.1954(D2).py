18d5532400dc6cda01121f497a8c99bbdcc20937
yunjaeeun <yunjaeeun12@naver.com>
2024-08-01 22:30:40 +0900

    24.08.01 실습 1. 달팽이

--- 99.algorithm/swea/D2/no.1954(D2).py
+++ 99.algorithm/swea/D2/no.1954(D2).py
@@ -1,40 +1,30 @@
 T = int(input())
-# 0  1  2  3
-# 우 하 좌 상
-di = [0, 1, 0, -1]
-dj = [1, 0, -1, 0]
-
 for tc in range(1, T + 1):
-    N = int(input())  # 달팽이 크기
+    N = int(input())
 
     snail = [[0] * N for _ in range(N)]
-    # 방향 dr[0] = 우, dr[1] = 하
-    d = 0
-    # 현재 위치 (0, 0)에서 시작
-    r, c = 0, 0
 
+    current_i = 0
+    current_j = 0
+    current_move = 'right'
     for i in range(1, N * N + 1):
-        # 방향을 언제 바꿔야 할까?
-        if r > N - 1 or c > N - 1 or snail[r][c] != 0:
-            if d == 3:
-                r -= di[d]
-                c -= dj[d]
-                d = 0
-
-            else:
-                r -= di[d]
-                c -= dj[d]
-                d += 1
-                r += di[d]
-                c += dj[d]
-                snail[r][c] = i
-        else:
-            snail[r][c] = i
-            print(snail)
-            r += di[d]
-            c += dj[d]
-
-    print(snail)
-
-        # 배열의 범위를 벗어났을때
-        # 내가 이전에 놓은 숫자를 만났을떄
+        snail[current_i][current_j] = i
+        if current_move == 'right' and current_j + 1 < N and snail[current_i][current_j + 1] == 0:
+            current_j += 1
+            if current_j + 1 >= N or snail[current_i][current_j + 1] != 0:
+                current_move = 'down'
+        elif current_move == 'down' and current_i + 1 < N and snail[current_i + 1][current_j] == 0:
+            current_i += 1
+            if current_i + 1 >= N or snail[current_i + 1][current_j] != 0:
+                current_move = 'left'
+        elif current_move == 'left' and current_j - 1 >= 0 and snail[current_i][current_j - 1] == 0:
+            current_j -= 1
+            if current_j - 1 < 0 or snail[current_i][current_j - 1] != 0:
+                current_move = 'up'
+        elif current_move == 'up' and current_i - 1 >= 0 and snail[current_i - 1][current_j] == 0:
+            current_i -= 1
+            if current_i - 1 < 0 or snail[current_i - 1][current_j] != 0:
+                current_move = 'right'
+    print(f"#{tc}")
+    for i in range(N):
+        print(*snail[i])

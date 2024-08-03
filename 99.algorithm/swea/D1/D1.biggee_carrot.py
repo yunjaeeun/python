fa83d9efc3c23116d5d51af15588c1466c79ceae
yunjaeeun <yunjaeeun12@naver.com>
2024-08-03 23:09:41 +0900

    24.08.03 공부내용

---
+++ 99.algorithm/swea/D1/D1.biggee_carrot.py
@@ -0,0 +1,30 @@
+'''
+4
+5
+1 2 3 4 5
+5
+4 5 1 2 3
+5
+5 4 3 2 1
+8
+1 2 1 2 3 1 2 1
+
+'''
+T = int(input())
+
+for tc in range(1, T + 1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    count = 1
+    result = 1
+    for i in range(1, N):
+        if arr[i - 1] < arr[i]:
+            count += 1        
+        elif arr[i - 1] > arr[i]:
+            count = 1
+        
+        if count > result:
+            result = count
+        
+    
+    print(f"#{tc} {result}")
